d7d6a67aa89318a7eb1867aaaa471e8fcc4e752c
Anthony Brown <awebrown@gmail.com>
2016-11-19 11:06:59 -0500

    testing new name

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,11 +2,11 @@
 
 let React = require('react'),
     Weather = require('Weather');
-    
+
 let WeatherMessage = ({temp, location}) => {
     return (
       <div>
-        <h3 className="text-center">It is {temp}&deg;F in {location}.</h3>
+        <h3 className="text-center">It is {temp}&deg;F in {location}!!</h3>
       </div>
     );
   }
