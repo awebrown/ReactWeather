2f17c28106064e87e4d45793e521ae7f73f0d16f
Anthony Brown <awebrown@gmail.com>
2016-11-19 01:59:20 -0500

    added cop

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ let React = require('react');
 
 let Examples = (props) => {
     return (
-      <h3>Examples component!</h3>
+      <div>
+        <h3>Examples component!</h3>
+        <p>Welcome to examples page.</p>
+      </div>
     )
   };
 
